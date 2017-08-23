b6231c28c0c385ec19a0a6638fd8b110b8a467ce
Nguyen Van Vuong <euclid1990@gmail.com>
2017-08-23 17:16:02 +0700

    Fix UT of App component after add Auth module routing

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -23,10 +27,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render nav tab', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('ul.nav-tabs > li > a').textContent).toContain('Dashboard');
   }));
 });
